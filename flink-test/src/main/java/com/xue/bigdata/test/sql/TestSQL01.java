d09c72ed583aabcdcbd9d9c69c740cfaaa134a51
xuemingwei <xuemingwei@heytea.com>
2022-08-17 15:18:05 +0800

    add join test

--- flink-test/src/main/java/com/xue/bigdata/test/sql/TestSQL01.java
+++ flink-test/src/main/java/com/xue/bigdata/test/sql/TestSQL01.java
@@ -1,4 +1,4 @@
-package com.xue.bigdata.test.cdc;
+package com.xue.bigdata.test.sql;
 
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
@@ -19,19 +19,22 @@ public class TestSQL01 {
                 "  `shop_name` STRING,\n" +
                 "  `no` STRING,\n" +
                 "  `created_at` BIGINT,\n" +
-                "  `updated_at` BIGINT,\n" +
-                "  `dispatched_at` BIGINT\n" +
+                "  `time_ltz` AS TO_TIMESTAMP_LTZ(`created_at`, 3)," +
+                // "  WATERMARK FOR time_ltz AS time_ltz - INTERVAL '5' SECOND" + // 指定事件事件
+                "  time_proc AS PROCTIME()" + // 指定处理时间
                 ") WITH (\n" +
                 "  'connector' = 'kafka',\n" +
                 "  'topic' = 'ods_db_production__mt_orders',\n" +
                 "  'properties.bootstrap.servers' = '" + parameter.get("bootstrap.servers") + "',\n" +
                 "  'properties.group.id' = 'test-xue',\n" +
-                "  'scan.startup.mode' = 'timestamp',\n" +
-                "  'scan.startup.timestamp-millis' = '1660096800000',\n" +
+                "  'scan.startup.mode' = 'latest-offset',\n" +
+//                "  'scan.startup.mode' = 'timestamp',\n" +
+//                "  'scan.startup.timestamp-millis' = '1660096800000',\n" +
                 "  'format' = 'debezium-json'\n" +
                 ")";
         tableEnv.executeSql(createSourceSql);
 
-        tableEnv.executeSql("select * from mt_orders where `no` = '151976173342425047'").print();
+        tableEnv.executeSql("select * from mt_orders").print();
+        // tableEnv.executeSql("SELECT TUMBLE_START(time_ltz, INTERVAL '1' MINUTE), COUNT(DISTINCT shop_id) FROM mt_orders GROUP BY TUMBLE(time_ltz, INTERVAL '1' MINUTE)").print();
     }
 }
